30c172fdf9506e6cd30d70a4193e37b26b95b3c2
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-05-24 04:57:44 +0000

    leAMrne24 new l144gi744

--- Inheritence/method_overloading.java
+++ Inheritence/method_overloading.java
@@ -1,7 +1,27 @@
 
 package Inheritence;
 
+
 class method_overloading {
     
-    
+    static int sum(int a,int b){
+        return a+b;
+    }
+    static int sum(int a,int b,int c){
+        return a+b;
+    }
+    static String sum(String a,String b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        
+     System.out.println(sum(3,4));
+     System.out.println(sum(3,4,3));
+     System.out.println(sum("hello","world"));
+
+    }
+
+
+
 }
