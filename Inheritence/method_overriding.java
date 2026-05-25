07ec213f0bec98f160f2cd9b4a352b919f7879f7
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-05-25 07:52:51 +0000

    lePMrne25 new l145gi151

--- Inheritence/method_overriding.java
+++ Inheritence/method_overriding.java
@@ -2,7 +2,17 @@ package Inheritence;
 
 class BANK {
     void RateOfInterest(){
-        System.out.println("Rate of Interest of bank is ");
+        System.out.println("Rate of Interest of bank is 7%");
+    }
+}
+class SBI {
+    void RateOfInterest(){
+        System.out.println("Rate of Interest of SBI is 10%");
+    }
+}
+class PNB {
+    void RateOfInterest(){
+        System.out.println("Rate of Interest of PNB is 7%");
     }
 }
 
