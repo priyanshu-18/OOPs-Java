a107602c980543ab67505dd3346f1d33fc87e707
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-05-26 10:15:01 +0000

    lePMrne26 new l146gi21

--- HashMap/hashmap.java
+++ HashMap/hashmap.java
@@ -61,7 +61,7 @@ public class hashmap {
             Set<String> key = map.keySet();
 
             for(String keys : key){
-                System.out.println(key+" "+map.get(key));
+                System.out.println(keys+" "+map.get(keys));
             }
 
     }
